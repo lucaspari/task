2b7b7ae6fde070451f09077a93612d1222e8c6e7
lucaspari <lucas.pari@icloud.com>
2025-01-14 17:05:36 -0300

    feat: Remove HelloWorld component and update Menu component for improved user roles display, also changed parser

--- client/src/components/Menu.vue
+++ client/src/components/Menu.vue
@@ -2,39 +2,23 @@
   <v-data-table
     :headers="headers"
     :items="users"
-    :items-per-page="5"
-    class="elevation-1"
-    :footer-props="{
-      'items-per-page-options': [5, 10, 15],
-    }"
-  >
-    <template v-slot:top>
-      <v-toolbar flat>
-        <v-toolbar-title>Users</v-toolbar-title>
-      </v-toolbar>
-    </template>
-    <template v-slot:item.actions="{ item }">
-      <v-btn class="mr-2" color="primary">Edit</v-btn>
-      <v-btn color="error">Delete</v-btn>
-    </template>
-  </v-data-table>
+    density="compact"
+    item-key="name"
+  ></v-data-table>
 </template>
-
 <script lang="ts">
 import axios from "axios";
-import { defineComponent } from "vue";
-
-export default defineComponent({
+export default {
   data() {
     return {
       users: [],
       headers: [
-        { text: "Username", value: "username" },
-        { text: "Timezone", value: "preferences" },
-        { text: "Is Active?", value: "active" },
-        { text: "Last Update At", value: "update_ts" },
-        { text: "Created At", value: "created_ts" },
-        { text: "Actions", value: "actions", sortable: false },
+        { title: "username", align: "start", sortable: false, key: "username" },
+        { title: "Roles", align: "start", sortable: false, key: "roles" },
+        { title: "Is Active?", align: "end", key: "active" },
+        { title: "Timezone", align: "end", key: "preferences" },
+        { title: "Last Update At", align: "end", key: "update_ts" },
+        { title: "Created At", align: "end", key: "created_ts" },
       ],
     };
   },
@@ -44,10 +28,12 @@ export default defineComponent({
   methods: {
     async fetchUsers() {
       axios.get("http://localhost:8000/users").then((response) => {
-        console.log(response.data);
+        response.data.map((user) => {
+          user.roles = user.roles.join(", ");
+        });
         this.users = response.data;
       });
     },
   },
-});
+};
 </script>
--- parser.py
+++ parser.py
@@ -4,14 +4,17 @@ from datetime import datetime
 
 def parse_user_data_to_db(data: dict) -> User:
     current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    roles = []
+    if data.get('is_user_admin') == True:
+        roles.append("admin")
+    if data.get('is_user_manager') == True:
+        roles.append("manager")
+    if data.get('is_user_tester') == True:
+        roles.append("tester")
     user = User(
         username=data.get('user', ''),  
         password=data.get('password', ''),  
-        roles={
-            "is_user_admin": True if data.get('is_user_admin') else False,
-            "is_user_manager": True if data.get('is_user_manager') else False, 
-            "is_user_tester": True if data.get('is_user_tester') else False 
-        },
+        roles=roles,
         preferences=UserPreferences(data.get('user_timezone')).timezone,  
         active=True if data.get('is_user_active') else False,  
         created_ts=data.get('created_at', ''),  
@@ -23,11 +26,7 @@ def parse_user_data_to_server(data: dict) -> User:
     user = User(
         username=data.get('username', ''),  
         password=data.get('password', ''),  
-                roles={
-            "is_user_admin": "is_user_admin -> admin" if data["roles"]["is_user_admin"] else "",
-            "is_user_manager": "is_user_manager -> manager" if data["roles"]["is_user_manager"] else "",
-            "is_user_tester": "is_user_tester -> tester" if data["roles"]["is_user_tester"] else ""
-        },
+                roles=data.get('roles'),
         preferences=data.get('preferences'),  
         active=True if data.get('active', False) else False,  
         created_ts=data.get('created_ts', ''),  
